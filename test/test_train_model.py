94514f3de20a42bb7710a85064e8b553f038734c
highlyprofitable108 <mikefusc@me.com>
2023-05-12 19:56:54 -0400

    Create test_train_model.py

---
+++ test/test_train_model.py
@@ -0,0 +1,24 @@
+import unittest
+import pandas as pd
+import numpy as np
+from src.train_model import train_model
+
+class TestTrainModel(unittest.TestCase):
+
+    def test_train_model(self):
+        # Create a sample DataFrame for testing
+        data = pd.DataFrame({
+            'strokes_gained': np.random.rand(100),
+            'course_length': np.random.randint(400, 600, 100)
+        })
+
+        target = data['strokes_gained'] * 2 + data['course_length'] * 3 + np.random.rand(100)
+
+        # Apply train_model function
+        model = train_model(data, target)
+
+        # Assert that the model has been trained, i.e., it has coefficients
+        self.assertTrue(hasattr(model, 'coef_'))
+
+if __name__ == '__main__':
+    unittest.main()
