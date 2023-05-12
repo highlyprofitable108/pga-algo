fcd58924508265621999732e6c36235d6e2cc2a0
highlyprofitable108 <mikefusc@me.com>
2023-05-12 17:54:21 -0400

    Update test_preprocess_data.py

--- test/test_preprocess_data.py
+++ test/test_preprocess_data.py
@@ -5,11 +5,15 @@ from src.preprocess_data import preprocess_data
 def test_preprocess_data():
     # Test case 1: Check that missing values are handled correctly
     data = pd.DataFrame({'A': [1, 2, np.nan], 'B': ['a', 'b', 'c']})
+    print("Original Data:\n", data)
     processed_data = preprocess_data(data)
+    print("/n/nProcessed Data:\n", data)
     assert processed_data.isnull().sum().sum() == 0, "Missing values were not handled correctly"
 
     # Test case 2: Check that categorical variables are encoded correctly
     # Adjust this test case based on how your function handles categorical variables
     data = pd.DataFrame({'A': [1, 2, 3], 'B': ['a', 'b', 'c']})
+    print("Original Data:\n", data)
     processed_data = preprocess_data(data)
+    print("/n/nProcessed Data:\n", data)
     assert processed_data['B'].dtype == 'int', "Categorical variables were not encoded correctly"
