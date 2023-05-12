5299e714c355b856f157a912d3dc3132d1779382
highlyprofitable108 <mikefusc@me.com>
2023-05-12 16:54:10 -0400

    Update create_features.py

--- src/create_features.py
+++ src/create_features.py
@@ -12,42 +12,44 @@ from src.create_features import create_features
 from src.train_model import train_model
 from datetime import datetime
 
-# Function to preprocess the input data (e.g., encoding categorical variables, scaling numerical variables)
-def preprocess_data(data):
+# TODO: Implement function for feature engineering
+def create_features(data):
     """
-    Preprocess the data, including handling missing values, encoding categorical variables,
-    and scaling numerical variables.
+    Create new features from the existing data.
     
     :param data: pandas DataFrame containing the golf data
-    :return: pandas DataFrame with preprocessed data
+    :return: pandas DataFrame with new features added
     """
+    # Create interaction features between variables
+    data['strokes_gained_per_course_length'] = data['strokes_gained'] / data['course_length']
+    data['strokes_gained_per_hazards'] = data['strokes_gained'] / data['hazards']
 
-    # Handle missing values
-    # Replace missing numerical values with the median value of the column
-    for col in data.select_dtypes(include=np.number):
-        data[col].fillna(data[col].median(), inplace=True)
+    # Calculate the average temperature during the tournament
+    data['avg_temperature'] = (data['temp_min'] + data['temp_max']) / 2
 
-    # Replace missing categorical values with the mode of the column
-    for col in data.select_dtypes(include='category'):
-        data[col].fillna(data[col].mode()[0], inplace=True)
+    # Convert wind speed to wind force category using the Beaufort scale
+    data['wind_force'] = pd.cut(data['wind_speed'],
+                                bins=[0, 1, 4, 7, 11, 17, 24, 31, 39, 47, 56, 64, float('inf')],
+                                labels=list(range(0, 12)))
 
-    # Encode categorical variables
-    # One-hot encode categorical variables with multiple categories
-    data = pd.get_dummies(data, drop_first=True)
-
-    # Scale numerical variables
-    # Normalize numerical variables to the range [0, 1]
-    for col in data.select_dtypes(include=np.number):
+    # Feature scaling (normalize the data to the range [0, 1])
+    for col in ['strokes_gained', 'course_length', 'hazards', 'avg_temperature', 'wind_speed']:
         data[col] = (data[col] - data[col].min()) / (data[col].max() - data[col].min())
 
     return data
-
 # TODO:
-# 1. Experiment with different strategies for handling missing values, such as using
-#    mean or mode for numerical variables or using a model-based imputation method.
-# 2. Explore other encoding methods for categorical variables, such as ordinal encoding
-#    or target encoding, to see if they improve the model's performance.
-# 3. Investigate different feature scaling techniques, like standardization or log
-#    transformation, and their impact on the model's performance.
-# 4. Consider using dimensionality reduction techniques, such as PCA or t-SNE, to
-#    reduce the number of features and improve the model's efficiency.
+# 1. Investigate additional interaction features: Explore other meaningful interactions
+#    between variables that could improve the model's performance.
+# 2. Investigate polynomial features: Instead of just looking at interactions, consider
+#    creating polynomial features (squared, cubic, etc.) of the existing variables.
+# 3. Incorporate player-specific features: Include player-specific features such as
+#    player ranking, age, or past performance to capture individual differences.
+# 4. Create categorical features for weather: Convert continuous weather variables like
+#    temperature, wind speed, and humidity into categorical features (e.g., temperature range)
+#    to capture non-linear relationships.
+# 5. Perform feature selection: Use techniques like recursive feature elimination or
+#    regularization methods (Lasso, Ridge) to identify the most important features for the model.
+# 6. Use domain knowledge to create additional relevant features: Leverage your understanding
+#    of golf to create features that could be relevant to player performance.
+# 7. Experiment with different feature scaling methods: Try standardization or other
+#    scaling techniques to see if they improve the model's performance.
