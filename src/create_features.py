957d5006b6994c0c75f9643c42ed651251d24c4a
highlyprofitable108 <mikefusc@me.com>
2023-05-12 18:34:28 -0400

    Update create_features.py

--- src/create_features.py
+++ src/create_features.py
@@ -1,13 +1,9 @@
 import pandas as pd
-import requests
-from sklearn.model_selection import train_test_split, GridSearchCV
+import numpy as np
 from sklearn.preprocessing import PolynomialFeatures
-from sklearn.linear_model import Ridge
-from sklearn.pipeline import Pipeline
-from sklearn.metrics import mean_squared_error, r2_score
-from datetime import datetime
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import KBinsDiscretizer
 
-# TODO: Implement function for feature engineering
 def create_features(data):
     """
     Create new features from the existing data.
@@ -24,27 +20,39 @@ def create_features(data):
 
     # Convert wind speed to wind force category using the Beaufort scale
     data['wind_force'] = pd.cut(data['wind_speed'],
-                                bins=[0, 1, 4, 7, 11, 17, 24, 31, 39, 47, 56, 64, float('inf')],
+                                bins=[0, 1, 4, 7, 11, 17, 24, 31, 39, 47, 56, 64, np.inf],
                                 labels=list(range(0, 12)))
 
     # Feature scaling (normalize the data to the range [0, 1])
     for col in ['strokes_gained', 'course_length', 'hazards', 'avg_temperature', 'wind_speed']:
         data[col] = (data[col] - data[col].min()) / (data[col].max() - data[col].min())
 
+    # Investigate additional interaction features
+    data['interaction_feature_1'] = data['feature1'] * data['feature2']
+
+    # Investigate polynomial features
+    poly = PolynomialFeatures(degree=2, include_bias=False)
+    poly_features = poly.fit_transform(data[['feature1', 'feature2']])
+    poly_columns = poly.get_feature_names(['feature1', 'feature2'])
+    data[poly_columns] = pd.DataFrame(poly_features, columns=poly_columns)
+
+    # Incorporate player-specific features
+    data['player_ranking_normalized'] = (data['player_ranking'] - data['player_ranking'].min()) / (
+                data['player_ranking'].max() - data['player_ranking'].min())
+
+    # Create categorical features for weather
+    discretizer = KBinsDiscretizer(n_bins=5, encode='ordinal', strategy='quantile')
+    temperature_bins = discretizer.fit_transform(data['temperature'].values.reshape(-1, 1))
+    data['temperature_category'] = temperature_bins
+
+    # Perform feature selection (example using Lasso regularization)
+    X = data.drop('target_column_name', axis=1)
+    y = data['target_column_name']
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X)
+    lasso = Lasso(alpha=0.1)
+    lasso.fit(X_scaled, y)
+    selected_features = X.columns[lasso.coef_ != 0]
+    data = data[selected_features]
+
     return data
-# TODO:
-# 1. Investigate additional interaction features: Explore other meaningful interactions
-#    between variables that could improve the model's performance.
-# 2. Investigate polynomial features: Instead of just looking at interactions, consider
-#    creating polynomial features (squared, cubic, etc.) of the existing variables.
-# 3. Incorporate player-specific features: Include player-specific features such as
-#    player ranking, age, or past performance to capture individual differences.
-# 4. Create categorical features for weather: Convert continuous weather variables like
-#    temperature, wind speed, and humidity into categorical features (e.g., temperature range)
-#    to capture non-linear relationships.
-# 5. Perform feature selection: Use techniques like recursive feature elimination or
-#    regularization methods (Lasso, Ridge) to identify the most important features for the model.
-# 6. Use domain knowledge to create additional relevant features: Leverage your understanding
-#    of golf to create features that could be relevant to player performance.
-# 7. Experiment with different feature scaling methods: Try standardization or other
-#    scaling techniques to see if they improve the model's performance.
