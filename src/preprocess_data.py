c4b107d3be911f36a47fb5b84c86a5026a17673d
highlyprofitable108 <mikefusc@me.com>
2023-05-12 17:44:54 -0400

    Update preprocess_data.py

--- src/preprocess_data.py
+++ src/preprocess_data.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import requests
-import numpy np
+import numpy as np
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import Ridge
