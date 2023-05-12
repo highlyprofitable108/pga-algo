e29a12db75fbf7fdc55828f3d09b314c01e6a36a
highlyprofitable108 <mikefusc@me.com>
2023-05-12 16:53:48 -0400

    Update create_features.py

--- src/create_features.py
+++ src/create_features.py
@@ -1,1 +1,53 @@
+import pandas as pd
+import requests
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import Ridge
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import mean_squared_error, r2_score
 
+# Import custom functions
+from src.preprocess_data import preprocess_data
+from src.create_features import create_features
+from src.train_model import train_model
+from datetime import datetime
+
+# Function to preprocess the input data (e.g., encoding categorical variables, scaling numerical variables)
+def preprocess_data(data):
+    """
+    Preprocess the data, including handling missing values, encoding categorical variables,
+    and scaling numerical variables.
+    
+    :param data: pandas DataFrame containing the golf data
+    :return: pandas DataFrame with preprocessed data
+    """
+
+    # Handle missing values
+    # Replace missing numerical values with the median value of the column
+    for col in data.select_dtypes(include=np.number):
+        data[col].fillna(data[col].median(), inplace=True)
+
+    # Replace missing categorical values with the mode of the column
+    for col in data.select_dtypes(include='category'):
+        data[col].fillna(data[col].mode()[0], inplace=True)
+
+    # Encode categorical variables
+    # One-hot encode categorical variables with multiple categories
+    data = pd.get_dummies(data, drop_first=True)
+
+    # Scale numerical variables
+    # Normalize numerical variables to the range [0, 1]
+    for col in data.select_dtypes(include=np.number):
+        data[col] = (data[col] - data[col].min()) / (data[col].max() - data[col].min())
+
+    return data
+
+# TODO:
+# 1. Experiment with different strategies for handling missing values, such as using
+#    mean or mode for numerical variables or using a model-based imputation method.
+# 2. Explore other encoding methods for categorical variables, such as ordinal encoding
+#    or target encoding, to see if they improve the model's performance.
+# 3. Investigate different feature scaling techniques, like standardization or log
+#    transformation, and their impact on the model's performance.
+# 4. Consider using dimensionality reduction techniques, such as PCA or t-SNE, to
+#    reduce the number of features and improve the model's efficiency.
