1cb4055ab6ac20265dcdf6183bed056a67bbfa69
highlyprofitable108 <mikefusc@me.com>
2023-05-12 19:28:57 -0400

    Update create_features.py

--- src/create_features.py
+++ src/create_features.py
@@ -1,11 +1,3 @@
-import unittest
-import pandas as pd
-import numpy as np
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import KBinsDiscretizer
-from sklearn.linear_model import Lasso
-
 def create_features(data):
     """
     Create new features from the existing data.
@@ -29,15 +21,6 @@ def create_features(data):
     for col in ['strokes_gained', 'course_length', 'hazards', 'avg_temperature', 'wind_speed']:
         data[col] = (data[col] - data[col].min()) / (data[col].max() - data[col].min())
 
-    # Investigate additional interaction features
-    # data['interaction_feature_1'] = data['feature1'] * data['feature2']
-
-    # Investigate polynomial features
-    # poly = PolynomialFeatures(degree=2, include_bias=False)
-    # poly_features = poly.fit_transform(data[['feature1', 'feature2']])
-    # poly_columns = poly.get_feature_names(['feature1', 'feature2'])
-    # can you get medata[poly_columns] = pd.DataFrame(poly_features, columns=poly_columns)
-
     # Incorporate player-specific features
     data['player_ranking_normalized'] = (data['player_ranking'] - data['player_ranking'].min()) / (
                 data['player_ranking'].max() - data['player_ranking'].min())
@@ -48,14 +31,13 @@ def create_features(data):
     data['temperature_category'] = temperature_bins
 
     # Perform feature selection (example using Lasso regularization)
-    X = data['strokes_gained'].values.reshape(-1,1)
-    y = data[['strokes_gained', 'course_length']]  # Separate the target columns
+    X = data[['strokes_gained', 'course_length']].values
+    y = data[['strokes_gained']]  # Separate the target column
     scaler = StandardScaler()
     X_scaled = scaler.fit_transform(X)
     lasso = Lasso(alpha=0.1)
     lasso.fit(X_scaled, y)
-    selected_features = ['strokes_gained']
-    data = pd.concat([data[selected_features], y], axis=1)  # Include the target columns in the result
+    selected_features = ['strokes_gained', 'course_length']
+    data = data[selected_features]  # Include the selected features in the result
 
     return data
-
