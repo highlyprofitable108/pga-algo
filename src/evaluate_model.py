a2d594eb573d66fdf2475bef807c2b127e368120
highlyprofitable108 <mikefusc@me.com>
2023-05-12 23:30:32 -0400

    Update evaluate_model.py

--- src/evaluate_model.py
+++ src/evaluate_model.py
@@ -1,8 +1,3 @@
-import matplotlib.pyplot as plt
-import seaborn as sns
-import numpy as np
-from sklearn.metrics import mean_absolute_error, mean_squared_error, confusion_matrix, roc_curve, auc
-
 def evaluate_model(models, X_test, y_test):
     """
     Evaluate the performance of an ensemble of models on a test set.
@@ -10,7 +5,7 @@ def evaluate_model(models, X_test, y_test):
     :param models: a list of trained models
     :param X_test: pandas DataFrame, test data (features)
     :param y_test: pandas Series, test data (target)
-    :return: None
+    :return: MAE, RMSE, and Predicted values
     """
     # Make predictions on the test set with each model
     predictions = [model.predict(X_test) for model in models]
@@ -27,10 +22,11 @@ def evaluate_model(models, X_test, y_test):
     print(f'Root Mean Squared Error (RMSE): {rmse:.2f}')
 
     # Plot actual vs predicted values
-    plt.scatter(y_test, y_pred)
+    plt.scatter(y_test, y_pred, alpha=0.3)
+    plt.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], 'k--', lw=2)
     plt.xlabel('Actual Scores')
     plt.ylabel('Predicted Scores')
     plt.title('Actual vs Predicted Golf Scores')
     plt.show()
 
-    return mae, rmse
+    return mae, rmse, y_pred
