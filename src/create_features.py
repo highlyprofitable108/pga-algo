f2a1ddc374e34b882943337a3e481db893dde8c8
highlyprofitable108 <mikefusc@me.com>
2023-05-13 18:27:35 -0400

    Update create_features.py

--- src/create_features.py
+++ src/create_features.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import KBinsDiscretizer
 from sklearn.linear_model import Lasso
 
 def create_features(data):
@@ -13,6 +12,14 @@ def create_features(data):
     :return: pandas DataFrame with new features added
     """
     try:
+        # Checking if the data is None or empty
+        if data is None or data.empty:
+            raise ValueError("Input data is None or empty")
+            
+        # Checking if 'sg_total' is in the dataframe
+        if 'sg_total' not in data.columns:
+            raise ValueError("'sg_total' is not in the dataframe")
+
         selected_data = data.copy()  # Create a copy of the original data
 
         if 'sg_putt' in selected_data.columns:
@@ -53,7 +60,7 @@ def create_features(data):
         X_scaled = scaler.fit_transform(X)
         lasso = Lasso(alpha=0.1)
         lasso.fit(X_scaled, y)
-        feature_coef = pd.Series(lasso.coef_, index=X.columns)
+                feature_coef = pd.Series(lasso.coef_, index=X.columns)
         selected_features = feature_coef[feature_coef != 0].index.tolist()
 
         # Include only the selected features in the result
@@ -64,3 +71,15 @@ def create_features(data):
     except Exception as e:
         print(f"Error occurred during feature creation: {str(e)}")
         return None
+
+# Incorporate weather data: As suggested in the function, you can fetch historical weather data based on the date and location of each event, and add this data as new features. Weather conditions like temperature, precipitation, and wind speed might have significant effects on golf games.
+# 
+# Incorporate player-specific data: If you have access to player-specific data, such as player's age, physical condition, or historical performance, you might consider adding these as new features as well.
+# 
+# Use more advanced feature selection methods: The current function uses Lasso regression for feature selection, which is a simple and commonly used method. But depending on your specific needs, you might want to use more sophisticated methods, such as Recursive Feature Elimination, SelectFromModel, or others.
+# 
+# Hyperparameter tuning: The alpha parameter for the Lasso model is currently set to a fixed value of 0.1. You might want to use GridSearchCV or other hyperparameter tuning methods to find the optimal value.
+# 
+# Automate feature engineering: Instead of manually creating interaction features and polynomial features, you could use automated feature engineering tools like Featuretools.
+# 
+# Handling of missing values: Currently, missing values are replaced with appropriate representations (like the mean). You might want to explore more sophisticated imputation methods, such as KNN imputation or multivariate imputation by chained equations (MICE).
